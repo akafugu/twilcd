1b4cdc581d793cd4f4b098ee869c245797ba9761
Karl Backström <backstrom@akafugu.jp>
2012-04-08 13:09:44 +0900

    Added copyright and some instructions for RGB test sketch

--- TWILiquidCrystal/examples/simple/simple.ino
+++ TWILiquidCrystal/examples/simple/simple.ino
@@ -1,3 +1,17 @@
+/*
+ * Simple test code
+ * (C) 2012 Akafugu Corporation
+ *
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ */
 
 #include <Wire.h>
 #include "TWILiquidCrystal.h"
@@ -10,11 +24,6 @@ LiquidCrystal lcd(m_addr);
 void setup() {                
   Wire.begin();
   
-  // initialize the digital pin as an output.
-  // Pin 13 has an LED connected on most Arduino boards:
-  pinMode(13, OUTPUT);
-  
-  delay(100);
   // set up the LCD's number of columns and rows: 
   lcd.begin(16, 2);
   // Print a message to the LCD.
--- TWILiquidCrystal/examples/test_RGB/test_RGB.ino
+++ TWILiquidCrystal/examples/test_RGB/test_RGB.ino
@@ -1,3 +1,21 @@
+/*
+ * RGB test code
+ * (C) 2012 Akafugu Corporation
+ *
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ */
+
+// This sketch is for the TWILCD 40x2/40x4/RGB edition 
+// (http://store.akafugu.jp/products/33) together with a 
+// RGB backlit display.
 
 #include <Wire.h>
 #include "TWILiquidCrystal.h"
